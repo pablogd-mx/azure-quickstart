589f32b55c422e9b75953921e344ce45477413a0
Pablo <pablo.diaz@mendix.com>
2022-06-16 14:39:57 +0200

    Cleaning up Environment values

--- azure/env-aks.sh
+++ azure/env-aks.sh
@@ -1,26 +1,24 @@
 #AKS parameters
-export AZ_LOCATION=westeurope
-export AZ_RESOURCE_GROUP=mendix-test
-export AZ_CLUSTER_NAME=mendix-quickstart
-export AZ_ACR=pablok8sreg
+export AZ_LOCATION=TODO
+export AZ_RESOURCE_GROUP=TODO
+export AZ_CLUSTER_NAME=TODO
+export AZ_ACR=TODO #name of the registry in Azure that Mendix will use to store its images
 
 #Connected Mode
-export CLUSTER_ID=f2d74384-fed8-4cf0-b00b-90ed1e7d3eff
-export CLUSTER_SECRET=ID65Wj1qA5SAbxXG
-export AKS_NS_Connected=quickstart-connected
+export CLUSTER_ID= TODO
+export CLUSTER_SECRET= TODO
+export AKS_NS_Connected=TODO #Namespace to create for Connected Mode installation
 
 #Standalone Mode
-export AKS_NS_Standalone=quickstart-standalone
+export AKS_NS_Standalone=TODO
 
 #Tekton
 export PATH_TO_HELM=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P) #Current working directory
-export URL_TO_YOUR_REPO_WITHOUT_TAG=pablok8sreg.azurecr.io
-#export YOUR_NAMESPACE=quickstart-aks #Use StandaloneNS
-#export YOUR_NAMESPACE_WITH_PIPELINES=quickstart-aks
-export SOME_UNIQUE_NAME=aks-tkn
-export TEKTON_NAMESPACE=tekton-pipelines
+export URL_TO_YOUR_REPO_WITHOUT_TAG=TODO #ACR registry to store tekton images.
+export SOME_UNIQUE_NAME= TODO # Name for tekton pods
+export TEKTON_NAMESPACE= TODO # Tekton pipeline namespace
 
 #MXPC
-export MXPC_VERSION=2.5.1
-export MXPC_OS=macos-amd64
+export MXPC_VERSION=TODO #Version of MXPC to download
+export MXPC_OS=TODO # version of OS from where the mxpc-cli will be lauched. For example macos-amd64
 
